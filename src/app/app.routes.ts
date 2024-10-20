966a54dfe795b7a0aabfab0b947afbaa4f118362
Elisabeth <elise.doisneau@gmail.com>
2024-10-20 18:55:59 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about-me', component: AboutMeComponent },
+];
